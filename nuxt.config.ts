cb99c5da484a60c0055a0b82459be99a20c03635
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 11:29:27 +0100

    fix: set base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -9,6 +8,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ]
-    }
+    },
+    baseURL: process.env.BASE_URL || '/',
   }
 })
