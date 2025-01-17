48dfad038a6edc63e0bdaf4970a472c062fb9646
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 21:16:44 +0100

    refactor: move fonts and main.css back to assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/public/css/main.css'],
+  css: ['~/assets/css/main.css'],
   app: {
     head: {
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-        { rel: 'preload', href: '/fonts/OpenSans-Regular.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
-        { rel: 'preload', href: '/fonts/OpenSans-Bold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' }
       ]
     }
   }
