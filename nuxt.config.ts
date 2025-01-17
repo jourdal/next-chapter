40bab9bdaeb869282d2e729057b7e037e6f10df9
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 10:44:10 +0100

    build: set ssr to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  ssr: true,
+  ssr: false,
   app: {
     head: {
       link: [
