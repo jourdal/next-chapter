8fe2a52bf46744804880433af33b947388fee05b
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 20:07:26 +0100

    fix: move main.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ['~/public/css/main.css'],
   app: {
     head: {
       link: [
