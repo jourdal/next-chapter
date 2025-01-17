21982302ac020b3c4e7670bd0bf60869447d825e
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 19:50:51 +0100

    build: try to preload fonts with visual indicator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -7,6 +6,8 @@ export default defineNuxtConfig({
     head: {
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'preload', href: '/fonts/OpenSans-Regular.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' },
+        { rel: 'preload', href: '/fonts/OpenSans-Bold.ttf', as: 'font', type: 'font/ttf', crossorigin: 'anonymous' }
       ]
     }
   }
