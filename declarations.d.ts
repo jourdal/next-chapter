65c7426e9ee0358946327ba698607e510d64aba6
Jo Urdal <jo.urdal@telenor.no>
2025-01-17 21:08:14 +0100

    fix: add .css declaration

--- declarations.d.ts
+++ declarations.d.ts
@@ -12,3 +12,8 @@ declare module '*.ico' {
     const content: string;
     export default content;
 }
+
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+}
