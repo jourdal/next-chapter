ec986079bfe374efc3d313fb9acbb865e7f35011
Jo Urdal <jo.urdal@telenor.no>
2025-01-18 12:18:48 +0100

    fix: style changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,20 +3,17 @@
 
 <template>
   <div class="container">
-    <iframe class="" :src="`https://www.instagram.com/p/DESEDPBNUHh/embed`" width="400" height="480" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
+    <iframe class="instagram-video" :src="`https://www.instagram.com/p/DESEDPBNUHh/embed`" width="400" height="480" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
   </div>
 </template>
 
 <style scoped>
 .container {
-  align-items: center;
-  display: flex;
-  flex-direction: column;
   gap: 1rem;
   justify-content: center;
   height: 100vh;
 
-  iframe {
+  .instagram-video {
     border: 0;
     border-radius: 1rem;
     box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
