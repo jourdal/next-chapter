9aa74a7d74a269b33b1bae1d9d0181493bf343bb
Jo Urdal <jo.urdal@telenor.no>
2025-01-18 11:52:12 +0100

    feat: remove home page title

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
 
 <template>
   <div class="container">
-    <h2>Welcome to the AdVenture!</h2>
     <iframe :src="`https://www.instagram.com/p/DESEDPBNUHh/embed`" width="400" height="480" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
   </div>
 </template>
