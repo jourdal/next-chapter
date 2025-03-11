4e003d751c6db2174a6153ca184a1a4c5a871d67
Jo Urdal <jo.urdal@telenor.no>
2025-03-11 19:01:29 +0100

    feat: remove env check

--- server/api/signUp.ts
+++ server/api/signUp.ts
@@ -6,22 +6,9 @@ const sendGridApiKey = process.env.SENDGRID_API_KEY;
 const kvRestApiUrl = process.env.KV_REST_API_URL;
 const kvRestApiToken = process.env.KV_REST_API_TOKEN;
 
-if (!sendGridApiKey) {
-    console.error('SENDGRID_API_KEY is not defined');
-    throw new Error('SENDGRID_API_KEY is not defined');
-}
-
-if (!kvRestApiUrl || !kvRestApiToken) {
-    console.error('KV_REST_API_URL or KV_REST_API_TOKEN is not defined');
-    throw new Error('KV_REST_API_URL or KV_REST_API_TOKEN is not defined');
-}
-
 sgMail.setApiKey(sendGridApiKey);
 
 export default eventHandler(async (event: H3Event) => {
-    console.log('Handler invoked');
-    console.log('Request method:', event.node.req.method);
-
     if (event.node.req.method === 'POST') {
         const { email } = await readBody(event);
         console.log('Request body:', { email });
