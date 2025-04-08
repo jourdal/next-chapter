16a42ba3eefdbe92f0c0094da6136eec501d3d7b
Jo Urdal <jo.urdal@telenor.no>
2025-04-08 19:42:43 +0200

    feat: improve layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,29 @@
 <template>
   <VApp>
     <Header />
-      <VMain class="layout-container">
-        <slot />
-      </VMain>
+    <VMain class="layout-container" >
+      <slot />
+    </VMain>
     <Footer />
   </VApp>
 </template>
 
+<script setup lang="ts">
+import { useHead } from 'nuxt/app';
+
+useHead({
+  titleTemplate: 'NextChapter',
+  meta: [
+    { name: 'description', content: 'Default layout for the NextChapter app' },
+  ],
+});
+</script>
+
 <style scoped>
 .layout-container {
   width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 </style>
