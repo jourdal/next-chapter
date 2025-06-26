e0958ac7e43c38aa1d3a271c666bec900a093e5c
Jo Urdal <jo.urdal@telenor.no>
2025-06-26 21:09:49 +0200

    feat: add nuxt content + blog page poc

--- pages/Blog.vue
+++ pages/Blog.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-
+const route = useRoute()
+const { data: blogposts } = await useAsyncData(route.path, () => {
+  return queryCollection('blog').all();
+})
 </script>
 
 <template>
-
+  <VContainer class="container">
+    <p v-for="post in blogposts" :key="post.id">
+      <NuxtLink :to="post.path">{{ post.title }}</NuxtLink>
+    </p>
+  </VContainer>
 </template>
 
 <style scoped>
-
+.container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
 </style>
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data: blogpost } = await useAsyncData(route.path, () => {
+  return queryCollection('blog').path(route.path).first();
+})
+</script>
+
+<template>
+  <VContainer class="container">
+    <ContentRenderer v-if="blogpost" :value="blogpost" class="content" />
+  </VContainer>
+</template>
+
+<style scoped>
+.container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+
+  .content {
+    white-space: pre-line;
+  }
+}
+
+.container :deep(hr) {
+  margin: 1rem 0;
+}
+</style>
