bd531de468f347a52b2e04803a2c337d10576a29
Jo Urdal <jo.urdal@telenor.no>
2025-07-02 17:54:43 +0200

    fix: typo

--- components/Banner.vue
+++ components/Banner.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="banner">
     <span>Join the adventure – weekly newsletters straight to your inbox! </span>
-    <VBtn variant="tonal" rounded="xl" append-icon="mdi-arrow-right" :to="{ path: '/newsletter' }" component="NuxtLink">Subscrie</VBtn>
+    <VBtn variant="tonal" rounded="xl" append-icon="mdi-arrow-right" :to="{ path: '/newsletter' }" component="NuxtLink">Subscribe</VBtn>
   </div>
 </template>
 
