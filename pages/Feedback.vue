72ac7ade13b118339768f04c1ca2c91ca97ac793
Jo Urdal <jo.urdal@telenor.no>
2025-07-03 11:07:16 +0200

    feat: add feedback page text

--- pages/Feedback.vue
+++ pages/Feedback.vue
@@ -3,9 +3,20 @@
 </script>
 
 <template>
-
+  <VContainer class="container">
+    <p>Do you have feedback, suggestions, or just something you want to share?</p>
+    <p>We really appreciate all feedback!</p>
+    <p>Send us an email at <a href="mailto:contact@nextchapter.space">contact@nextchapter.space</a>.</p>
+  </VContainer>
 </template>
 
 <style scoped>
-
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 80vh;
+  gap: 0.5rem;
+}
 </style>
