b54287462b172409c89fe10728a4fc9a484e5235
Jo Urdal <jo.urdal@telenor.no>
2025-07-05 14:05:14 +0200

    feat: use copy text in feedback page

--- pages/Feedback.vue
+++ pages/Feedback.vue
@@ -6,7 +6,7 @@
   <VContainer class="container">
     <p class="text">Do you have feedback, suggestions, or just something you want to share?</p>
     <p class="text">We really appreciate all feedback!</p>
-    <p class="text">Send us an email at <a href="mailto:contact@nextchapter.space">contact@nextchapter.space</a>.</p>
+    <CopyText class="copy-text" text="Send us an email at contact@nextchapter.space" />
   </VContainer>
 </template>
 
